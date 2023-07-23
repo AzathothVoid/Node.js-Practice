a4c9016d52637bd9c9a76b952a4fb9fa85ef1110
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2023-07-23 07:20:46 +0500

    Adding commentation

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,17 +1,27 @@
+//importing the express library for node.js
 var express = require("express");
+
+//importing the dotenv library that is used to load the environment variables from the .env file, allowing for the
+//use of proccess.env function
 require("dotenv").config();
+
+//initializing express
 var app = express();
 
+//setting the port number on which the server will listen
 var port = 3000;
 
+//setting absolute path for the front-end website resources to be loaded
 absolute_path = __dirname + "/public";
 
+//root level middleware function that displays a string of information about
+//request type, path and ip address of the requestee
 app.use(function (req, res, next) {
-  //logg = req.method + " " + req.path + " - " + req.ip;
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 
+//get route for displaying date via chaining middleware functions
 app.get(
   "/now",
   function (req, res, next) {
@@ -23,16 +33,20 @@ app.get(
   }
 );
 
+//practice route
 app.get("/", function (req, res) {
   res.send("Hello Express");
 });
 
+//practicing using env variables and the json function of res object
 app.get("/json", function (req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase")
     res.json({ message: "HELLO JSON" });
   else res.json({ message: "hello json" });
 });
 
+//Using inbuilt express middelware function for loading HTML and CSS Files from public folder
 app.use("/index", express.static(absolute_path));
 
+//Initializing server and making it listen on the port numbre set above
 app.listen(port);
