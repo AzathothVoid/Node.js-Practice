ca65458afaaae014a9d1e1ffd58b685b34f9a834
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2023-07-25 12:08:50 +0500

    completed timestamp logic

--- TimeStamp.js
+++ TimeStamp.js
@@ -4,9 +4,46 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-app.get("/")
+function isNumeric(str) {
+  return !isNaN(str);
+}
 
+app.get("/:date?", (request, response) => {
+  const params = request.params;
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log(`Server Listening on port ${listener.address().port}`);
+  const dateString = params.date;
+
+  var date = new Date();
+
+  if (!Boolean(dateString)) {
+    response.json({
+      Unix: Math.floor(date.getTime() / 1000),
+      UTC: date.toUTCString(),
+    });
+  }
+
+  if (isNumeric(dateString)) {
+    const milliseconds = parseInt(dateString);
+    date = new Date(milliseconds * 1000);
+
+    response.json({
+      Unix: milliseconds,
+      UTC: date.toUTCString(),
+    });
+  }
+
+  date = new Date(dateString);
+
+  if (!date.toJSON()) {
+    response.json({ err: "Invalid Date" });
+  }
+
+  response.json({
+    Unix: Math.floor(date.getTime() / 1000),
+    UTC: date.toUTCString(),
+  });
+});
+
+var listener = app.listen(3000, function () {
+  console.log(`Server Listening on port 3000`);
 });
